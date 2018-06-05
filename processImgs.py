bbe98c932d2fe555e295199587b447c649b8a42e
tkeesh <tkis.cro@gmail.com>
2018-06-05 12:14:36 +0200

    V1.2

--- processImgs.py
+++ processImgs.py
@@ -5,30 +5,34 @@ import os, argparse
 w, h = 227, 227
 
 
-def resizeMasked(datasetFolder, outputFolder):
+def resizeMasked(datasetDir, outputDir):
 	global w, h
 	if w == 0: s = 'w'
 	elif h == 0: s = 'h'
 	else: s = ''
-	loadPath = os.path.abspath(datasetFolder)
-	if len(outputFolder) > 0: savePath = os.path.abspath(outputFolder)
-	else: savePath = os.path.abspath(datasetFolder)
-	for fn in os.listdir(loadPath):
-		img = Image.open(os.path.join(loadPath, fn))
+
+	for fn in os.listdir(datasetDir):		
+		try: img = Image.open(os.path.join(datasetDir, fn))
+		except: continue
+
 		if len(s) > 0:
 			if s == 'w': w = img.size[0]
 			else: h = img.size[1]
-		if img.size[0] == w and img.size[1] == h: continue				
+
+		if img.size[0] == w and img.size[1] == h: 
+			continue
+		
 		if img.size[0] == w and img.size[1] < h:
 			black = Image.new(img.mode, (w, h), "black")
 			black.paste(img, (0, int(round((h-img.size[1])/2.0))))
-			black.save(os.path.join(savePath, fn))
+			black.save(os.path.join(outputDir, fn))
 			print(fn + " masked")
 			continue
+		
 		if img.size[0] < w and img.size[1] == h:
 			black = Image.new(img.mode, (w, h), "black")
 			black.paste(img, (int(round((w-img.size[0])/2.0)), 0))
-			black.save(os.path.join(savePath, fn))
+			black.save(os.path.join(outputDir, fn))
 			print(fn + " masked")
 			continue
 
@@ -41,49 +45,52 @@ def resizeMasked(datasetFolder, outputFolder):
 			if round(img.size[0]*fac) > w: fac2 = float(w) / round(img.size[0]*fac)
 			else: fac2 = 1
 		img = img.resize((int(round(img.size[0]*fac*fac2)), int(round(img.size[1]*fac*fac2))), Image.ANTIALIAS)
-		
+
 		black = Image.new(img.mode, (w, h), "black")
 		if img.size[0] == w:
 			black.paste(img, (0, int(round((h-img.size[1])/2.0))))
 		else:
 			black.paste(img, (int(round((w-img.size[0])/2.0)), 0))
-		black.save(os.path.join(savePath, fn))
+		black.save(os.path.join(outputDir, fn))
 		print(fn + " resized scaled and masked")
 
 
-def resizeForced(datasetFolder, outputFolder):
+def resizeForced(datasetDir, outputDir):
 	global w, h
 	if w == 0: s = 'w'
 	elif h == 0: s = 'h'
 	else: s = ''
-	loadPath = os.path.abspath(datasetFolder)
-	if len(outputFolder) > 0: savePath = os.path.abspath(outputFolder)
-	else: savePath = os.path.abspath(datasetFolder)
-	for fn in os.listdir(loadPath):
-		img = Image.open(os.path.join(loadPath, fn))
+
+	for fn in os.listdir(datasetDir):
+		try: img = Image.open(os.path.join(datasetDir, fn))
+		except: continue
+
 		if len(s) > 0:
 			if s == 'w': w = img.size[0]
-			else: h = img.size[1]		
+			else: h = img.size[1]
+
 		if img.size[0] == w and img.size[1] == h:
 			continue
-		
+
 		img = img.resize((w,h), Image.ANTIALIAS)
 		
-		img.save(os.path.join(savePath, fn))
+		img.save(os.path.join(outputDir, fn))
 		print(fn + " resized forced")
 
 
-def resizeScaled(datasetFolder, outputFolder):
+def resizeScaled(datasetDir, outputDir):
 	global w, h
-	loadPath = os.path.abspath(datasetFolder)
-	if len(outputFolder) > 0: savePath = os.path.abspath(outputFolder)
-	else: savePath = os.path.abspath(datasetFolder)
-	for fn in os.listdir(loadPath):
-		img = Image.open(os.path.join(loadPath, fn))
-		
+
+	for fn in os.listdir(datasetDir):
+		try: img = Image.open(os.path.join(datasetDir, fn))
+		except: continue
+
 		if w > 0 and h > 0:
-			if img.size[0] == w and img.size[1] <= h: continue
-			if img.size[0] < w and img.size[1] == h: continue
+			if img.size[0] == w and img.size[1] <= h: 
+				continue
+			if img.size[0] < w and img.size[1] == h: 
+				continue
+
 			if img.size[0] > img.size[1]:
 				fac = float(w) / img.size[0]
 				if round(img.size[1]*fac) > h: fac2 = float(h) / round(img.size[1]*fac)
@@ -102,43 +109,51 @@ def resizeScaled(datasetFolder, outputFolder):
 			fac = float(h) / img.size[1]
 			img = img.resize((int(round(img.size[0]*fac)), h), Image.ANTIALIAS)
 
-		img.save(os.path.join(savePath, fn))
+		img.save(os.path.join(outputDir, fn))
 		print(fn + " resized scaled")
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser("Handle images. Methods: masked, scaled, forced (default) - set irrelevant size to 0 to resize by 1D")
-	parser.add_argument("imagesFolder", type=str, help="path to images folder (output folder optional)", nargs='+')
+	parser = argparse.ArgumentParser("Transform images. Methods: masked, scaled, forced (default) - to resize by 1D set irrelevant size to 0")
+	parser.add_argument("imagesDir", type=str, help="path to images directory", nargs=1)
+	parser.add_argument("outputDir", type=str, help="output directory (optional)", nargs='?')
 	parser.add_argument("-m", action="store_true", help="masked - set irrelevant size to 0 to mask by 1D (image unresized)")
 	parser.add_argument("-s", action="store_true", help="scaled - set irrelevant size to 0 (default: fitted and scaled within w x h)")
-	
+
 	parser.add_argument("--wh", help="set output size (default: {0}, {1})".format(str(w), str(h)), type=int, nargs=2)
-	
+
 	#parser.add_argument("--rr", help="rotate right (default: {0}, {1})".format(str(w), str(h)), type=int, nargs=1)
-	#parser.add_argument("--rl", help="rotate left (default: {0}, {1})".format(str(w), str(h)), type=int, nargs=1) ne treba 
-	#parser.add_argument("--mirror u d", help="rotate right (default: {0}, {1})".format(str(w), str(h)), type=int, nargs=1) 
-	
-	args = parser.parse_args()	
-	if len(args.imagesFolder) == 2: 
-		outputFolder = args.imagesFolder[1]
-		if not os.path.exists(os.path.abspath(outputFolder)): os.makedirs(os.path.abspath(outputFolder))
-	elif len(args.imagesFolder) == 1: outputFolder = ''
-	else: raise argparse.ArgumentTypeError("Max number of paths is 2 (inputFolder, outputFolder)")
-	if not os.path.exists(os.path.abspath(args.imagesFolder[0])): raise OSError(2, 'No such file or directory', os.path.abspath(args.imagesFolder[0]))	
-	if args.wh: w, h = args.wh[0], args.wh[1]	
-	if w <= 0 and h <= 0: 
+	#parser.add_argument("--rl", help="rotate left (default: {0}, {1})".format(str(w), str(h)), type=int, nargs=1) ne treba -90
+	#parser.add_argument("--mirror u d", help="rotate right (default: {0}, {1})".format(str(w), str(h)), type=int, nargs=1)
+	#parser.add_argument("--mirrorUpsideDown u d", help="rotate right (default: {0}, {1})".format(str(w), str(h)), type=int, nargs=1)
+
+	args = parser.parse_args()
+
+	datasetDir = os.path.abspath(args.imagesDir[0])
+	if not os.path.exists(datasetDir): 
+		raise OSError(2, 'No such file or directory', datasetDir)
+
+	if args.outputDir:
+		outputDir = os.path.abspath(args.outputDir)
+	else: 
+		outputDir = datasetDir + '_out'
+	if not os.path.exists(outputDir): os.makedirs(outputDir)
+
+	if args.wh: w, h = args.wh[0], args.wh[1]
+
+	if w <= 0 and h <= 0:
 		print("Wrong size: w = {0}, h = {1}".format(str(w), str(h)))
 		raise SystemExit(0)
-	if args.m: 	
+
+	if args.m:
 		print("Method: masked")
 		print("w = {0}, h = {1}".format(str(w), str(h)))
-		resizeMasked(args.imagesFolder[0], outputFolder)	
+		resizeMasked(datasetDir, outputDir)
 	elif args.s:
 		print("Method: scaled")
 		print("w = {0}, h = {1}".format(str(w), str(h)))
-		resizeScaled(args.imagesFolder[0], outputFolder)
-	else: 		
+		resizeScaled(datasetDir, outputDir)
+	else:
 		print("Method: forced")
 		print("w = {0}, h = {1}".format(str(w), str(h)))
-		resizeForced(args.imagesFolder[0], outputFolder)
-
+		resizeForced(datasetDir, outputDir)
